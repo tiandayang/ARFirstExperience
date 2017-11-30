c09c0d37994ee883b4004bac5d5802564fad83b0
田向阳 <254711559@qq.com>
2017-11-30 15:08:36 +0800

    AR初体验之绘制立方体模型

---
+++ ARFirstExperience/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARFirstExperience
+//
+//  Created by 田向阳 on 2017/11/30.
+//  Copyright © 2017年 田向阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ARFirstExperience/ViewController.m
@@ -0,0 +1,89 @@
+//
+//  ViewController.m
+//  ARFirstExperience
+//
+//  Created by 田向阳 on 2017/11/30.
+//  Copyright © 2017年 田向阳. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <ARSCNViewDelegate>
+
+@property (nonatomic, strong) IBOutlet ARSCNView *sceneView;
+
+@end
+
+    
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Set the view's delegate
+    self.sceneView.delegate = self;
+    
+    // Show statistics such as fps and timing information
+    self.sceneView.showsStatistics = YES;
+    
+    // Create a new scene
+    SCNScene *scene = [[SCNScene alloc] init];
+    SCNBox *box = [SCNBox boxWithWidth:0.1 height:0.1 length:0.1 chamferRadius:0.0];
+    SCNNode *node = [SCNNode nodeWithGeometry:box];
+    node.position = SCNVector3Make(0, 0, -0.25);
+        // Set the scene to the view
+    [scene.rootNode addChildNode:node];
+    self.sceneView.scene = scene;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    // Create a session configuration
+    ARWorldTrackingConfiguration *configuration = [ARWorldTrackingConfiguration new];
+
+    // Run the view's session
+    [self.sceneView.session runWithConfiguration:configuration];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+    // Pause the view's session
+    [self.sceneView.session pause];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - ARSCNViewDelegate
+
+/*
+// Override to create and configure nodes for anchors added to the view's session.
+- (SCNNode *)renderer:(id<SCNSceneRenderer>)renderer nodeForAnchor:(ARAnchor *)anchor {
+    SCNNode *node = [SCNNode new];
+ 
+    // Add geometry to the node...
+ 
+    return node;
+}
+*/
+
+- (void)session:(ARSession *)session didFailWithError:(NSError *)error {
+    // Present an error message to the user
+    
+}
+
+- (void)sessionWasInterrupted:(ARSession *)session {
+    // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    
+}
+
+- (void)sessionInterruptionEnded:(ARSession *)session {
+    // Reset tracking and/or remove existing anchors if consistent tracking is required
+    
+}
+
+@end
